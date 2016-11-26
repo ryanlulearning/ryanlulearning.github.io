a0345cf5af1522d5763c236587b560cbc18c5e04
ryanlu <zhiyelu@usc.edu>
2016-11-25 20:43:03 -0800

    added Katex support

---
+++ assets/themes/twitter/js/process-katex.js
@@ -0,0 +1,18 @@
+/**
+ * Replace  mathjax directives with katex directives to render latex equations
+ */
+$("script[type='math/tex']").replaceWith(
+  function(){
+    var tex = $(this).text();
+    return "<span class=\"inline-equation\">" +
+           katex.renderToString(tex) +
+           "</span>";
+});
+
+$("script[type='math/tex; mode=display']").replaceWith(
+  function(){
+    var tex = $(this).text();
+    return "<div class=\"equation\">" +
+           katex.renderToString("\\displaystyle "+tex) +
+           "</div>";
+});
